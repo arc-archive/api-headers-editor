0e97e7ccdc9f8ade4e4390abaca1c065e35bd00b
Pawel Psztyc <jarrodek@gmail.com>
2019-01-02 18:33:55 -0800

    Updating polymer docs

--- api-headers-editor.d.ts
+++ api-headers-editor.d.ts
@@ -8,6 +8,10 @@
  *   api-headers-editor.html
  */
 
+
+// tslint:disable:variable-name Describing an API that's defined elsewhere.
+// tslint:disable:no-any describes the API as best we are able today
+
 /// <reference path="../polymer/types/polymer-element.d.ts" />
 /// <reference path="../polymer/types/lib/elements/dom-if.d.ts" />
 /// <reference path="../polymer/types/lib/utils/render-status.d.ts" />
